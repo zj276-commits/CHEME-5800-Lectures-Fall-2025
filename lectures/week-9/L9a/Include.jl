e8dbd076430aa94291343eabf65dbfc73958984c
Jeffrey Varner <jdv27@cornell.edu>
2025-10-19 14:29:31 -0400

    Add Julia environment and update Perceptron example notebook
    Added Include.jl and Project.toml to set up a Julia environment for the Perceptron example in week 9, lecture L9a. Updated the notebook title and execution count for clarity and reproducibility.

---
+++ lectures/week-9/L9a/Include.jl
@@ -0,0 +1,31 @@
+# setup paths -
+const _ROOT = @__DIR__;
+const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# if we are missing any packages, install them -
+using Pkg;
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load external packages -
+using VLDataScienceMachineLearningPackage
+using LinearAlgebra
+using BenchmarkTools
+using Statistics
+using Test
+using Images
+using TestImages
+using ImageMagick
+using ImageIO
+using DelimitedFiles
+using Plots
+using DataFrames
+using Random
+using Distributions
+using PrettyTables
+
+# setup random number generator -
+Random.seed!(1234); # seed the random number generator for reproducibility
