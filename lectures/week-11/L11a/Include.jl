0a2ae36937f84f7d5307383cb01b7fa438d4df3c
Jeffrey D Varner <jdv27@cornell.edu>
2025-10-31 16:16:19 -0400

    Add Markov model lecture and example for week 11
    Introduced new lecture and example materials for week 11 on Markov models, including Jupyter notebooks, figures, Julia code, and supporting files. The example demonstrates construction of a three-state Markov chain, computation and validation of its stationary distribution, and simulation of Markov chain dynamics. Supporting project files and HTML exports are also included.

---
+++ lectures/week-11/L11a/Include.jl
@@ -0,0 +1,21 @@
+# setup paths -
+const _ROOT = @__DIR__
+const _PATH_TO_SRC = joinpath(_ROOT, "src");
+
+# check: do we need to download any packages?
+using Pkg
+if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
+    Pkg.activate(".");
+    Pkg.add(path="https://github.com/varnerlab/VLDataScienceMachineLearningPackage.jl.git")
+    Pkg.resolve(); Pkg.instantiate(); Pkg.update();
+end
+
+# load the required packages -
+using VLDataScienceMachineLearningPackage
+using Distributions
+using Plots
+using Colors
+using LinearAlgebra
+using Statistics
+using DataFrames
+using PrettyTables
